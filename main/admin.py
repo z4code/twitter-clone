dbd309326b00f16f81d20dc96be946a7e21fb066
Diyorbek Qodirboyev <d.zcode4@gmail.com>
2024-09-27 07:18:34 -0700

    profile model has been created

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
 
-# Register your models here.
+admin.site.unregister(Group)
+admin.site.unregister(User)
+
+class ProfileInline(admin.StackedInline):
+	model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+	model = User
+	fields = ['username']
+	inlines = [ProfileInline]
+
+admin.site.register(User, UserAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	follows = models.ManyToManyField(User, related_name='followed_by', symmetrical=False, blank=True)
+
+	def __str__(self):
+		return self.user.username
