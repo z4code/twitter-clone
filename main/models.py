e6a14b6fed505bfbcf54db569ee92904d0ab3d2f
Diyorbek Qodirboyev <d.zcode4@gmail.com>
2024-09-27 08:16:31 -0700

    automatic user profile creation

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,3 +8,12 @@ class Profile(models.Model):
 
 	def __str__(self):
 		return self.user.username
+
+def create_profile(sender, instance, created, **kwargs):
+	if created:
+		user_profile = Profile(user=instance)
+		user_profile.save()
+
+		user_profile.follows.set([instance.profile.id])
+		user_profile.save()
+post_save.connect(create_profile, sender=User)
